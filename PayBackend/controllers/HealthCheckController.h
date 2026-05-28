373d235dd1f22a985d4ef4e1ea8c638b278cfca5
vilas <vilas900420@gmail.com>
2026-05-28 17:35:01 +0800

    feat: add /healthz, /readyz endpoints with /health compat alias

--- PayBackend/controllers/HealthCheckController.h
+++ PayBackend/controllers/HealthCheckController.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <drogon/HttpController.h>
+#include <atomic>
 
 using namespace drogon;
 
@@ -8,8 +9,28 @@ class HealthCheckController : public drogon::HttpController<HealthCheckControlle
 {
   public:
     METHOD_LIST_BEGIN
+    ADD_METHOD_TO(HealthCheckController::healthz, "/healthz", Get, Options);
+    ADD_METHOD_TO(HealthCheckController::readyz, "/readyz", Get, Options);
     ADD_METHOD_TO(HealthCheckController::health, "/health", Get, Options);
     METHOD_LIST_END
 
-    void health(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+    void healthz(
+      const HttpRequestPtr &req,
+      std::function<void(const HttpResponsePtr &)> &&callback
+    );
+
+    void readyz(
+      const HttpRequestPtr &req,
+      std::function<void(const HttpResponsePtr &)> &&callback
+    );
+
+    void health(
+      const HttpRequestPtr &req,
+      std::function<void(const HttpResponsePtr &)> &&callback
+    );
+
+  private:
+    std::atomic<int> consecutiveFailures_{0};
+    std::atomic<bool> lastReadyState_{true};
+    static constexpr int FAILURE_THRESHOLD = 3;
 };
